816a71e8ed7235e833b0cbe6df610118fbe75120
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 18:58:24 +0330

    Feat: Create User Entity

---
+++ OrderManagement.Domain/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagement.Domain.Enums;
+
+namespace OrderManagement.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+
+        public string Username { get; private set; } = null!;
+
+        public string PasswordHash { get; private set; } = null!;
+
+        public UserRole Role { get; private set; }
+
+
+        private User()
+        {
+        }
+
+        public User(
+            string username,
+            string passwordHash,
+            UserRole role)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            PasswordHash = passwordHash;
+            Role = role;
+        }
+    }
+}
---
+++ OrderManagement.Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Enums
+{
+    public enum UserRole
+    {
+        User = 1,
+        Admin = 2
+    }
+}
