4b05fb21b323773e9b454088d899ddfa60cdd6c9
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 19:00:13 +0330

    Seed Data For Admin User

---
+++ OrderManagement.Infrastructure/Persistence/Seed/UserSeeder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Domain.Enums;
+using OrderManagement.Infrastructure.Data;
+
+namespace OrderManagement.Infrastructure.Persistence.Seed
+{
+    public static class UserSeeder
+    {
+       
+        public static async Task SeedAsync(
+            ApplicationDbContext context)
+        {
+            if (await context.Users.AnyAsync())
+                return;
+
+            var passwordHash =
+                BCrypt.Net.BCrypt.HashPassword("Admin123");
+
+            var admin = new User(
+                "admin",
+                passwordHash,
+                UserRole.Admin);
+
+            await context.Users.AddAsync(admin);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
