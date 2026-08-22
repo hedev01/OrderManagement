9188d8ba757136e82439f7da145aa03d00f13f36
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 19:01:14 +0330

    Feat: Create Unit Of Work

---
+++ OrderManagement.Application/Interfaces/Persistence/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Interfaces.Persistence
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default);
+
+        Task BeginTransactionAsync(
+            CancellationToken cancellationToken = default);
+
+        Task CommitTransactionAsync(
+            CancellationToken cancellationToken = default);
+
+        Task RollbackTransactionAsync(
+            CancellationToken cancellationToken = default);
+    }
+}
