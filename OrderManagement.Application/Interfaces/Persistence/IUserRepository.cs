0d952341c53474551d393f32687d79a4107c0c03
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 19:01:30 +0330

    Feat: User Repository

---
+++ OrderManagement.Application/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagement.Domain.Entities;
+
+namespace OrderManagement.Application.Interfaces.Persistence
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByUsernameAsync(
+            string username,
+            CancellationToken cancellationToken = default);
+
+        Task AddAsync(
+            User user,
+            CancellationToken cancellationToken = default);
+
+        Task<bool> ExistsByUsernameAsync(
+            string username,
+            CancellationToken cancellationToken = default);
+    }
+}
---
+++ OrderManagement.Infrastructure/Persistence/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Application.Interfaces.Persistence;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Infrastructure.Data;
+
+namespace OrderManagement.Infrastructure.Persistence
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<User?> GetByUsernameAsync(string username, CancellationToken cancellationToken = default)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(
+                    x => x.Username == username,
+                    cancellationToken); ;
+        }
+
+        public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+        {
+            await _context.Users.AddAsync(
+                user,
+                cancellationToken);
+        }
+
+        public async Task<bool> ExistsByUsernameAsync(string username, CancellationToken cancellationToken = default)
+        {
+            return await _context.Users
+                .AnyAsync(
+                    x => x.Username == username,
+                    cancellationToken);
+        }
+    }
+}
