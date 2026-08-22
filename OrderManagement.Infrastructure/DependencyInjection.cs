9d3f765397144a7ac58027da54559ddf61c477ec
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 18:57:42 +0330

    Feat: DI for Infrastructure

---
+++ OrderManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Application.Interfaces.Authentication;
+using OrderManagement.Application.Interfaces.Persistence;
+using OrderManagement.Infrastructure.Authentication;
+using OrderManagement.Infrastructure.Data;
+using OrderManagement.Infrastructure.Persistence;
+using OrderManagement.Infrastructure.Settings;
+
+namespace OrderManagement.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString =
+                configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+
+            
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+            services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+            return services;
+        }
+    }
+}
