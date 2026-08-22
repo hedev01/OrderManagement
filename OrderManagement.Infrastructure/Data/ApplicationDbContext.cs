0aaf6b37b87e16d29e080c296c8b4c1c3dfcc571
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 18:56:48 +0330

    Feat: Create ApplicationDbContext

---
+++ OrderManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Domain.Entities;
+
+namespace OrderManagement.Infrastructure.Data
+{
+   public sealed class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
