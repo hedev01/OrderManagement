02f6a1931a688c6170847260ef38647475220bd2
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 19:03:45 +0330

    Feat: Configure Application Infrastructure And Seed Initial Admin User

--- OrderManagement.API/Program.cs
+++ OrderManagement.API/Program.cs
@@ -1,13 +1,39 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using OrderManagement.Application;
+using OrderManagement.Infrastructure;
+using OrderManagement.Infrastructure.Authentication;
+using OrderManagement.Infrastructure.Data;
+using OrderManagement.Infrastructure.Persistence.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// DI For Infrastructure Layer
+builder.Services.AddInfrastructure(
+    builder.Configuration);
+builder.Services.AddJwtAuthentication(
+    builder.Configuration);
+// DI For Application Layer
+builder.Services.AddApplication();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context =
+        services.GetRequiredService<ApplicationDbContext>();
+
+    await UserSeeder.SeedAsync(context);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
