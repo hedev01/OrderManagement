6f201e37eafc9a4df3b9bbfc73da89798bdc3721
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 18:58:05 +0330

    Feat: DI For Application

---
+++ OrderManagement.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using OrderManagement.Application.Features.Authentication.CreateUser;
+using OrderManagement.Application.Features.Authentication.Login;
+
+namespace OrderManagement.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            services.AddScoped<ICreateUserUseCase, CreateUserUseCase>();
+            services.AddScoped<ILoginUseCase, LoginUseCase>();
+            return services;
+        }
+    }
+}
