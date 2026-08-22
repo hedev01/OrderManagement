49360cdc337ac7c9b527abff1d856230cc28cca3
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 23:11:42 +0330

    Feat: Add Customers DbSet

--- OrderManagement.Infrastructure/Data/ApplicationDbContext.cs
+++ OrderManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace OrderManagement.Infrastructure.Data
    public sealed class ApplicationDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
           : base(options) { }
 
