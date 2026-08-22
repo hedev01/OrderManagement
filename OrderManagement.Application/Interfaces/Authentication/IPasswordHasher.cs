a990bc23f37f1e0a1ae1c8a5aef9283f3518ef1f
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 19:00:54 +0330

    Feat: Password Hasher Service

---
+++ OrderManagement.Application/Interfaces/Authentication/IPasswordHasher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Interfaces.Authentication
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+
+        bool Verify(string password, string passwordHash);
+    }
+}
---
+++ OrderManagement.Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagement.Application.Interfaces.Authentication;
+
+namespace OrderManagement.Infrastructure.Authentication
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password,passwordHash);
+        }
+    }
+}
