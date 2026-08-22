a0d8344c5dc24d2b629bcd3ec783e268e51a2ce2
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 23:11:25 +0330

    Feat: Create Customer Repository

---
+++ OrderManagement.Application/Interfaces/Persistence/ICustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagement.Domain.Entities;
+
+namespace OrderManagement.Application.Interfaces.Persistence
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(
+            Guid id,
+            CancellationToken cancellationToken);
+
+        Task<bool> ExistsByEmailAsync(
+            string email,
+            CancellationToken cancellationToken);
+
+        Task AddAsync(
+            Customer customer,
+            CancellationToken cancellationToken);
+
+        Task<IReadOnlyList<Customer>> GetAllAsync(
+            CancellationToken cancellationToken);
+    }
+}
---
+++ OrderManagement.Infrastructure/Persistence/CustomerRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Application.Interfaces.Persistence;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Infrastructure.Data;
+
+namespace OrderManagement.Infrastructure.Persistence
+{
+    public sealed class CustomerRepository : ICustomerRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Customer?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _context.Customers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(
+                    customer => customer.Id == id,
+                    cancellationToken);
+        }
+
+        public async Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken)
+        {
+            return await _context.Customers
+                .AsNoTracking()
+                .AnyAsync(
+                    customer => customer.Email == email,
+                    cancellationToken);
+        }
+
+        public async Task AddAsync(Customer customer, CancellationToken cancellationToken)
+        {
+            await _context.Customers.AddAsync(
+                customer,
+                cancellationToken);
+        }
+
+        public async Task<IReadOnlyList<Customer>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Customers
+                .AsNoTracking()
+                .OrderBy(customer => customer.CreatedAt)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
