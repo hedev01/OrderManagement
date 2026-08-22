cb21ab50d2aad0dff83a67cb25708ebe76f2bb50
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 18:58:50 +0330

    Feat: Use Result Pattern

---
+++ OrderManagement.Application/Common/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Common
+{
+    public class Result<T>
+    {
+        public T? Data { get; init; }
+
+        public bool IsSuccess { get; init; }
+        public string? ErrorMessage { get; init; }
+
+        public static Result<T> Success(T value) => new Result<T>()
+        {
+            IsSuccess = true,
+            Data = value,
+        };
+
+        public static Result<T> Failure(string errorMessage) => new Result<T>()
+        {
+            IsSuccess = false,
+            ErrorMessage = errorMessage
+        };
+
+    }
+}
