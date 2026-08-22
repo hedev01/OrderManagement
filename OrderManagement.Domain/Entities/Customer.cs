32eef46e8a235704e1a4b69432267dd4acb292f5
Hedeve01 <nazari.fph@gmail.com>
2026-08-22 23:08:44 +0330

    Feat: Create Customer Entity

---
+++ OrderManagement.Domain/Entities/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; private set; }
+
+        public string FirstName { get; private set; } = null!;
+
+        public string LastName { get; private set; } = null!;
+
+        public string Email { get; private set; } = null!;
+
+        public string PhoneNumber { get; private set; } = null!;
+
+        public DateTime CreatedAt { get; private set; }
+
+
+        private Customer()
+        {
+        }
+
+        public Customer(
+            string firstName,
+            string lastName,
+            string email,
+            string phoneNumber)
+        {
+            Id = Guid.NewGuid();
+
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
