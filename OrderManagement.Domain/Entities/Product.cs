c713ca22467df00151a06d250ba2d0c5e5bb886f
Hedeve01 <nazari.fph@gmail.com>
2026-08-23 14:08:44 +0330

    Feat: Create Product Entity

---
+++ OrderManagement.Domain/Entities/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; } = null!;
+
+        public string Description { get; private set; } = null!;
+
+        public decimal Price { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public Inventory Inventory { get; private set; } = null!;
+
+        private Product()
+        {
+        }
+
+        public Product(
+            string name,
+            string description,
+            decimal price)
+        {
+            Id = Guid.NewGuid();
+
+            Name = name;
+            Description = description;
+            Price = price;
+
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
