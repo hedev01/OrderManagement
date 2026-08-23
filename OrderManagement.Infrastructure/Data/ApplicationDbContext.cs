c5b3df5dc2786008c18fdddd7e6d0d1348cfffa8
Hedeve01 <nazari.fph@gmail.com>
2026-08-23 14:07:14 +0330

    Feat: Set DbSets

--- OrderManagement.Infrastructure/Data/ApplicationDbContext.cs
+++ OrderManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -9,17 +9,32 @@ using OrderManagement.Domain.Entities;
 
 namespace OrderManagement.Infrastructure.Data
 {
-   public sealed class ApplicationDbContext : DbContext
+    public sealed class ApplicationDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+
         public DbSet<Customer> Customers { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-          : base(options) { }
+        public DbSet<Product> Products { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public DbSet<Inventory> Inventories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(
+            ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly);
         }
     }
 }
