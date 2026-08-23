2c2b0f324c4390da4ab66c423df52d6f800653bc
Hedeve01 <nazari.fph@gmail.com>
2026-08-23 14:00:07 +0330

    Feat: Create Page Result

---
+++ OrderManagement.Application/Common/PagedResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Application.Common
+{
+    public sealed record PagedResult<T>(
+        IReadOnlyList<T> Items,
+        int Page,
+        int PageSize,
+        int TotalCount)
+    {
+        public int TotalPages =>
+            (int)Math.Ceiling(
+                TotalCount / (double)PageSize);
+    }
+}
