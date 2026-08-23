7588d88dd96a518e5f28b1923309cbd4c17e072b
Hedeve01 <nazari.fph@gmail.com>
2026-08-23 14:06:32 +0330

    Feat: Create Product Repository

---
+++ OrderManagement.Application/Interfaces/Persistence/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OrderManagement.Domain.Entities;
+
+namespace OrderManagement.Application.Interfaces.Persistence
+{
+    public interface IProductRepository
+    {
+        Task<IReadOnlyList<Product>> GetByIdsAsync(
+            IReadOnlyCollection<Guid> ids,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ OrderManagement.Infrastructure/Persistence/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Application.Interfaces.Persistence;
+using OrderManagement.Domain.Entities;
+using OrderManagement.Infrastructure.Data;
+
+namespace OrderManagement.Infrastructure.Persistence
+{
+    public sealed class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IReadOnlyList<Product>> GetByIdsAsync(IReadOnlyCollection<Guid> ids, CancellationToken cancellationToken)
+        {
+            return await _context.Products
+                .Include(x => x.Inventory)
+                .Where(x => ids.Contains(x.Id))
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
