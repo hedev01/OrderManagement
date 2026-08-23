fbb998a0523ade49fe82d6cafb5bd18e995b8c0a
Hedeve01 <nazari.fph@gmail.com>
2026-08-24 01:19:44 +0330

    Feat: Use SerialLog

--- OrderManagement.API/Program.cs
+++ OrderManagement.API/Program.cs
@@ -1,18 +1,15 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+
 using OrderManagement.Application;
 using OrderManagement.Infrastructure;
 using OrderManagement.Infrastructure.Authentication;
-using OrderManagement.Infrastructure.Data;
 using OrderManagement.Infrastructure.Extensions;
-using OrderManagement.Infrastructure.Persistence.Seed;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-
+//Logging
+builder.Host.AddSerilogLogging();
 // DI For Infrastructure Layer
 builder.Services.AddInfrastructure(
     builder.Configuration);
