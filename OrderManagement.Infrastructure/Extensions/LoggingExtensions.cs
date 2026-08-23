e9ab4ae90805498a9fb12a0391a2ea675f930744
Hedeve01 <nazari.fph@gmail.com>
2026-08-24 01:24:53 +0330

    Configure Logging

---
+++ OrderManagement.Infrastructure/Extensions/LoggingExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace OrderManagement.Infrastructure.Extensions
+{
+    public static class LoggingExtensions
+    {
+        public static IHostBuilder AddSerilogLogging(
+            this IHostBuilder hostBuilder)
+        {
+            hostBuilder.UseSerilog(
+                (context, configuration) =>
+                {
+                    configuration
+                        .ReadFrom.Configuration(
+                            context.Configuration)
+                        .Enrich.FromLogContext();
+                });
+
+            return hostBuilder;
+        }
+    }
+}
